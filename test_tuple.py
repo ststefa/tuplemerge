6738db4919d658f82b20cd4cd3db887e842479e1
Stefan Steinert <ststefa@heldenzeit.net>
2020-11-23 18:30:25 +0100

    complement tests

--- test_tuple.py
+++ test_tuple.py
@@ -1,5 +1,56 @@
 import tuple
 import random
+import pytest
+
+
+def _generate_test_set():
+    """ Helper method to generate a random input dataset """
+    test_set = {}
+    test_set['minval'] = random.randint(0, 1000)
+    test_set['maxval'] = test_set['minval']+random.randint(0, 1000)
+    test_set['maxrange'] = random.randint(10, 100)
+    test_set['num'] = random.randint(0, 100000)
+    return test_set
+
+
+def test_generate_tuples():
+    tuples = tuple.generate_tuples(0, 10, 3, 10000)
+    lval_list = [x for (x, y) in tuples]
+    rval_list = [y for (x, y) in tuples]
+    # check edges
+    assert 0 in lval_list
+    assert 0 not in rval_list
+    assert 10 in rval_list
+    assert 10 not in lval_list
+    # check middle part
+    for i in range(1, 10):
+        assert i in lval_list
+        assert i in rval_list
+
+
+def test_verify_tuples():
+    assert tuple.verify_tuples([(1, 1), (2, 3)])
+    assert tuple.verify_tuples([(10, 20), (2, 3)])
+    with pytest.raises(Exception):
+        tuple.verify_tuples([(1, 2), (2, 3)])
+    with pytest.raises(Exception):
+        tuple.verify_tuples([(5, 10), (2, 3), (8, 12)])
+
+
+def test_sort_tuples():
+    assert tuple.sort_tuples([(1, 1), (2, 3)]) == [(1, 1), (2, 3)]
+    assert tuple.sort_tuples([(2, 3), (1, 1)]) == [(1, 1), (2, 3)]
+
+
+def test_count_tuples():
+    # simple tests
+    assert tuple.count_tuples([]) == 0
+    assert tuple.count_tuples([(1, 1)]) == 1
+    # run some random tests
+    for i in range(0, 10):
+        testset = _generate_test_set()
+        tuples = tuple.generate_tuples(**testset)
+        assert tuple.count_tuples(tuples) == testset['num']
 
 
 def test_overlap():
@@ -25,25 +76,6 @@ def test_merge():
     assert tuple._merge((3, 5), (2, 5)) == (2, 5)
     assert tuple._merge((3, 5), (2, 4)) == (2, 5)
     assert tuple._merge((3, 5), (4, 7)) == (3, 7)
-
-
-def test_gen():
-    tuples = tuple.generate_tuples(0, 10, 3, 10000)
-    lval_list = [x for (x, y) in tuples]
-    rval_list = [y for (x, y) in tuples]
-    assert 0 in lval_list
-    assert 0 not in rval_list
-    assert 10 in rval_list
-    assert 10 not in lval_list
-
-
-def _generate_test_set():
-    testset = {}
-    testset['minval'] = random.randint(0, 10000)
-    testset['maxval'] = testset['minval']+random.randint(0, 10000)
-    testset['maxrange'] = random.randint(100, 10000)
-    testset['num'] = random.randint(0, 100000)
-    return testset
 
 
 def test_naive_strategy():
